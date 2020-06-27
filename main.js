81d9b5e6f4cc52a248fa8760342698bdf17fccd8
Adam Major <adam.j.major@gmail.com>
2020-06-26 22:26:29 -0700

    Made dynamic grid part;  need to test

--- main.js
+++ main.js
@@ -2,20 +2,18 @@
 const btn_start = document.getElementById('btn_start');
 const btn_reset = document.getElementById('btn_reset');
 const cells = document.getElementById('cells');
+
 btn_reset.addEventListener('click', reset);
 btn_start.addEventListener('click', start);
 function start(e){
+    reset();
     let cells_per_side = prompt('How many cells per side?');
-    const cells = document.getElementById('cells');
     const cell_length = cells.offsetWidth / cells_per_side;
     alert (cell_length)
     makeGrid(cells_per_side, cells_per_side);
-    
-
-    
 }
 function reset(e){1
-    alert('Need to figure out what reset does...');
+    cells.textContent = "";
 }
 
 function makeGrid(rows, cols) {  
@@ -36,13 +34,14 @@ for (let c = 0; c < (rows * cols); c++) {
 
 function arrangeCells(rows,cols){
     //assign rows and cols to each of the div cells made in makeCells();
-    r = rows
-    for (let currentRow = 1; currentRow <= rows*cols; currentRow++) {
-        const cell = document.getElementById(currentRow);
-        alert(cell.textContent);
-        for (let currentCol = 0; currentCol < array.length; currentCol++) {
-           
-            
+        //alert(`cell: ${cell.textContent}`);
+    let i = 1;
+    for (let r = 1; r <= rows; r++) {
+        for (let c = 1; c <= cols; c++) {
+            const cell = document.getElementById(i);
+            cell.style.setProperty('grid-row', r);
+            cell.style.setProperty('grid-column', c);
+            i++;
         }
     }
 }
