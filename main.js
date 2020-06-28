c207247fa8744811636a8b44c1d739a1496ae666
Adam Major <adam.j.major@gmail.com>
2020-06-27 18:11:39 -0700

    Finish up for the most part.  Just making look pretty and adding options like toggle border

--- main.js
+++ main.js
@@ -1,35 +1,88 @@
+const optionsEnum = {
+    Draw: 'd',
+    Erase: 'e',
+    Nothing: 'nothing', 
+    Reset: 'r',
+    Save: 'v',
+    Start: 's',
+    Toggle: 't',
+}
+const optionsEnumNames = {
+    Draw: 'Draw',
+    Erase: 'Erase',
+    Nothing: 'nothing', 
+    Reset: 'Reset',
+    Save: 'Save',
+    Start: 'Start',
+    Toggle: 'Toggle',
+}
+let options = [optionsEnum.Draw, optionsEnum.Erase, optionsEnum.Nothing, optionsEnum.Reset, optionsEnum.Save, optionsEnum.Start, optionsEnum.Toggle]
+
+let mincells_per_side = 16;
+let maxcells_per_side = 110;
+let showBorder = true;
+let mode = 'mouseover';
+let cells_per_side = 16, cntrl_pressed = optionsEnum.Nothing;
 
 const btn_start = document.getElementById('btn_start');
 const btn_reset = document.getElementById('btn_reset');
+const btn_save = document.getElementById('btn_save');
+const btn_toggle = document.getElementById('btn_toggle');
 const cells = document.getElementById('cells');
+const maxLength = getComputedStyle(document.documentElement).getPropertyValue('--max-length')
 
 btn_reset.addEventListener('click', reset);
+window.addEventListener('keypress', keyPressHandler);
+window.addEventListener('keyup', keyPressHandler);
 btn_start.addEventListener('click', start);
+btn_save.addEventListener('click', saveSketch);
+btn_toggle.addEventListener('click', toggleBorder);
+
+function changeBackground(e){
+    console.log(e);
+    // alert(cntrl_pressed);
+    if (cntrl_pressed == optionsEnum.Draw) {
+        e.originalTarget.classList.add('activated');
+    }
+    else if (cntrl_pressed == optionsEnum.Erase) {
+        e.originalTarget.classList.remove('activated');
+    }
+}
+
 function start(e){
-    reset();
-    let cells_per_side = prompt('How many cells per side?');
-    const cell_length = cells.offsetWidth / cells_per_side;
-    alert (cell_length)
+    cells.textContent = "";
+    let temp = prompt(`How many cells per side? (${mincells_per_side} - ${maxcells_per_side} inclusive are valid inputs)`);
+    if (!temp.match(/[0-9]+/i )|| cells_per_side > maxcells_per_side || cells_per_side < mincells_per_side) {
+        alert(`Enter and integer between ${mincells_per_side} and ${maxcells_per_side}.`)
+        return null
+    }
+    else {
+        cells_per_side = temp;
+    }
     makeGrid(cells_per_side, cells_per_side);
 }
-function reset(e){1
+
+function reset(e){
     cells.textContent = "";
+    makeGrid(cells_per_side, cells_per_side);   
 }
 
 function makeGrid(rows, cols) {  
     makeCells(rows,cols);  
     arrangeCells(rows,cols);
-    
-  };
+}
+
 function makeCells(rows, cols) {
-cells.style.setProperty('--grid-rows', rows);
-cells.style.setProperty('--grid-cols', cols);
-for (let c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.textContent = (c + 1);
-    cell.id = (c+1);
-    cells.appendChild(cell).className = "cell";
-};
+    cells.style.setProperty('--grid-rows', rows);
+    cells.style.setProperty('--grid-cols', cols);
+    for (let c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.setAttribute('draggable', true);
+        cell.addEventListener(mode, changeBackground);
+        //cell.textContent = (c + 1);
+        cell.id = `cell ${c+1}`;
+        cells.appendChild(cell).className = "cell-border";
+    };
 }
 
 function arrangeCells(rows,cols){
@@ -38,10 +91,112 @@ function arrangeCells(rows,cols){
     let i = 1;
     for (let r = 1; r <= rows; r++) {
         for (let c = 1; c <= cols; c++) {
-            const cell = document.getElementById(i);
+            const cell = document.getElementById(`cell ${i}`);
             cell.style.setProperty('grid-row', r);
             cell.style.setProperty('grid-column', c);
             i++;
         }
     }
 }
+
+function saveSketch(){
+    alert(`need to implement save still :)`);
+    var canvas = document.getElementById('canvas');
+    var canvas2d = canvas.getContext('2d');
+    const container = document.getElementsByClassName('cell');
+    var iter = 0;
+    const w = canvas.width / cells_per_side;
+    const h = canvas.height / cells_per_side;
+
+    for (let i = 0; i < cells_per_side; i++) {
+        for (let j = 0; j < cells_per_side; j++) {
+            canvas2d.fillStyle = container[iter].style.backgroundColor;
+            console.log(container[iter].style.backgroundColor);
+            let x = j * maxLength / cells_per_side;
+            let y = i * maxLength / cells_per_side;
+            canvas2d.fillRect(x, y, w, h);
+            iter++;
+        }
+    }
+    window.open(document.getElementById('canvas').toDataURL());
+}
+
+function keyPressHandler(e){
+    console.log(e);
+    if (e.type.toLowerCase() == 'keypress') {
+        if (e.key.toLowerCase() == optionsEnum.Start) {
+            start();
+        } 
+        else if (e.key.toLowerCase() == optionsEnum.Reset) {
+            reset();
+        }
+        else if (e.key.toLowerCase() == optionsEnum.Save) {
+            saveSketch();
+        }
+        else if (e.key.toLowerCase() == optionsEnum.Draw) {
+            cntrl_pressed = optionsEnum.Draw;
+        }
+        else if (e.key.toLowerCase() == optionsEnum.Erase) {
+            cntrl_pressed = optionsEnum.Erase;
+        }
+    }
+    else if (e.type.toLowerCase() == 'keyup'){
+        if (e.key.toLowerCase() == optionsEnum.Draw) {
+            cntrl_pressed = optionsEnum.Nothing;
+        }
+        else if (e.key.toLowerCase() == optionsEnum.Erase) {
+            cntrl_pressed = optionsEnum.Nothing;
+        }
+    }
+}
+
+function toggleBorder(e){
+    const cells = document.querySelectorAll('div[id^=cell');
+    for (let index = 0; index < cells.length; index++) {
+        const cell = cells[index];
+        //alert(cells.length);
+        console.log(cell.classList.contains('cell-borderless'));
+        if (cell.classList.contains('cell-borderless')) {
+            cell.classList.remove('cell-borderless');
+        } 
+        else {
+            cell.classList.add('cell-borderless');
+        }
+    }
+}
+
+function populateOptions() {
+    for(let option in optionsEnum){
+        alert(`${option} and ${optionsEnum[option]}`);
+        let optionLetter = optionsEnum[option];
+        if (option !== 'nothing') {
+            //create each div section below and add to div class="left-side">
+            createHtmlElement('div class="option-item"', 2, [`p class="p_draw">Hold '${optionLetter}' and Hover over a Square`])
+        
+             // <div class="option-item">
+            //     <p class="p_draw">Draw:</p>
+            //     <p class="p_draw">Hold 'd' and Hover Over a Square</p>
+            // </div>
+        }
+        else {
+            continue;
+        }
+    }
+   
+}
+
+function createHtmlElement (type, numberOfChildren = null, typeOfChildren = null) {
+    if (typeOfChildren.length != numberOfChildren) {
+        alert('The type of children and the array of children must ')
+    }
+    if (numberOfChildren && typeOfChildren) {
+        console.log(type,numberOfChildren, typeOfChildren)
+    }
+    else{
+        alert("No children");
+    }
+}
+
+makeGrid(cells_per_side,cells_per_side);
+//populateOptions();
+
